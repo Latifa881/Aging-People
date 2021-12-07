15a8fe6e5429f120bf0b455756add9cff2cbd2a9
Latifa881 <l-n-a-m@hotmail.com>
2021-12-07 11:21:16 -0800

    first commit

--- Aging People/ViewController.swift
+++ Aging People/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let names=["Latifa","Amal","Sara","Thunyan","Mohammad","Nasser"
+               ,"Noura","Shrifah","Mona","Muneera","Somaya","Turki"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
 
 
 }
 
+
+extension ViewController: UITableViewDataSource {
+    //Datasource
+    // How many cells are we going to need?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return an integer that indicates how many rows (cells) to draw
+        return names.count
+    }
+    
+    // How should I create each cell?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        
+        cell.textLabel?.text = names[indexPath.row]
+        cell.detailTextLabel?.text = "\(Int.random(in: 5...95)) years old"
+    
+        return cell
+    }
+    
+    
+    
+}
